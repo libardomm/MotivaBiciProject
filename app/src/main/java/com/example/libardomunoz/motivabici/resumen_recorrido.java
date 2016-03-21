c0a3527744896433fd7ec76b72f511abe1552cf2
Ronald Guiovanni Venegas Pulido <ronaldvenegasp@gmail.com>
2016-03-21 18:51:43 -0500

    Actividad "Calorías Quemadas" creada

--- app/src/main/java/com/example/libardomunoz/motivabici/resumen_recorrido.java
+++ app/src/main/java/com/example/libardomunoz/motivabici/resumen_recorrido.java
@@ -19,5 +19,13 @@ public class resumen_recorrido extends AppCompatActivity {
                 startActivity(new Intent(resumen_recorrido.this, PostLogin.class));
             }
         });
+
+        //Al tocar el texto "Calorías Quemadas" envía a la actividad de calorías
+        findViewById(R.id.caloriasTextView).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(resumen_recorrido.this, calorias.class));
+            }
+        });
     }
 }
